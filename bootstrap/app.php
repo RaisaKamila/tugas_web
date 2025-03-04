39a9ec36bedb54c9076272f3c61a43a8b1eab813
RaisaKamila <0081411167@smkn4padalarang.sch.id>
2025-03-04 16:29:14 +0700

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,15 +1,21 @@
 <?php
 
-namespace App\Http\Middleware;
-
-use Illuminate\Http\Middleware\TrustProxies as Middleware;
+use Illuminate\Foundation\Application;
+use Illuminate\Foundation\Configuration\Exceptions;
+use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
 
-class TrustProxies extends Middleware
-{
-    protected $proxies = '*'; // Percayai semua proxy
-
-    protected $headers = Request::HEADER_X_FORWARDED_FOR | 
-                         Request::HEADER_X_FORWARDED_HOST | 
-                         Request::HEADER_X_FORWARDED_PROTO;
-}
+return Application::configure(basePath: dirname(__DIR__))
+    ->withRouting(
+        web: __DIR__.'/../routes/web.php',
+        commands: __DIR__.'/../routes/console.php',
+        health: '/up',
+    )
+    ->withMiddleware(function (Middleware $middleware) {
+        $middleware->append(\App\Http\Middleware\TrustProxies::class);
+        $middleware->append(\App\Http\Middleware\ForceHttps::class); // Pastikan ForceHttps juga ada
+    })
+    ->withExceptions(function (Exceptions $exceptions) {
+        //
+    })
+    ->create();
