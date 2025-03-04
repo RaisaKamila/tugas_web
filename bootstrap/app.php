b63117c0a22821d26d6ac32dcf450f97030c2f08
RaisaKamila <0081411167@smkn4padalarang.sch.id>
2025-03-04 16:14:23 +0700

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,17 +3,19 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
 
-return Application::configure(basePath: dirname(__DIR__))
+return Application::configure(basePath: dirname(_DIR_))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: _DIR_.'/../routes/web.php',
+        commands: _DIR_.'/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->append(\App\Http\Middleware\TrustProxies::class);
+        $middleware->append(\App\Http\Middleware\ForceHttps::class); // Pastikan ForceHttps juga ada
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
-    })->create();
-    
+    })
+    ->create();
